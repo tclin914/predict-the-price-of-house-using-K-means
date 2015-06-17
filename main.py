5911790a9b9e4204e6fddc1cdf84e48d40a315da
tclin <tclin914@gmail.com>
2015-06-17 20:08:17 +0800

    modify for getting result

--- main.py
+++ main.py
@@ -31,68 +31,87 @@ with open('price.txt', 'rb') as f:
     priceSet = pickle.load(f)
 
 ##step 2: clustering...
+averageSet = []
 dataSet = mat(dataSet)
-k = 12
-train = dataSet[:len(dataSet) / 2]
-test = dataSet[len(dataSet) / 2:]
-test_priceSet = priceSet[len(priceSet) / 2:]
+for k in range(1, 101):
+    print k
+    train = dataSet[:len(dataSet) / 2]
+    test = dataSet[len(dataSet) / 2:]
+    test_priceSet = priceSet[len(priceSet) / 2:]
 
-centroids, clusterAssment = kmeans(train, k) 
+    centroids, clusterAssment = kmeans(train, k)    
 
-# distinguish
-test_index = [-1] * len(test)
-for i in range(0, len(test)):
-    min = -1
-    for j in range(k):
-        distance = euclDistance(centroids[j], test[i])
-        if min == -1:
-            min = distance
-            test_index[i] = j
-        elif distance < min:
-            min = distance
-            test_index[i] = j
-    #print test_index[i]
+    # distinguish
+    test_index = [-1] * len(test)
+    for i in range(0, len(test)):
+        min = -1
+        for j in range(k):
+            distance = euclDistance(centroids[j], test[i])
+            if min == -1:
+                min = distance
+                test_index[i] = j
+            elif distance < min:
+                min = distance
+                test_index[i] = j
+        #print test_index[i]
 
-# calculate average price per class in train data
-total = [0] * k
-count = [0] * k
-for i in range(0, len(train)):
-    index = int(clusterAssment[i, 0])
-    total[index] = total[index] + priceSet[i]
-    count[index] = count[index] + 1
+    # calculate average price per class in train data
+    total = [0] * k
+    count = [0] * k
+    for i in range(0, len(train)):
+        index = int(clusterAssment[i, 0])
+        total[index] = total[index] + priceSet[i]
+        count[index] = count[index] + 1
 
-# average per class
-average = [0] * k
-for i in range(k):
-    if count[i] != 0:
-        average[i] = total[i] / count[i]
-        #print average[i]
+    # average per class
+    average = [0] * k
+    for i in range(k):
+        if count[i] != 0:
+            average[i] = total[i] / count[i]
+            #print average[i]
 
-# calculate error rate
-error = [0] * len(test)
-for i in range(k):
-    for j in range(0, len(test)):
-        if test_index[j] == i:
-            print test_priceSet[j]
-            diff = fabs(test_priceSet[j] - average[i])
-            diff_percent = (diff / test_priceSet[j]) * 100
-            #print 'diff %f ' % diff_percent
-            error[j] = diff_percent
-    print 'average %s' % average[i]
+    # calculate error rate
+    error = [0] * len(test)
+    for i in range(k):
+        for j in range(0, len(test)):
+            if test_index[j] == i:
+                #print test_priceSet[j]
+                diff = fabs(test_priceSet[j] - average[i])
+                diff_percent = (diff / test_priceSet[j]) * 100
+                if diff_percent > 200:
+                    diff_percent = 0
 
-# calculus average error
-average_error = 0
-total_error = 0
-for e in error:
-    total_error = total_error + e
-print 'average error rate %f ' % (total_error / len(test))
+                #print 'diff %f ' % diff_percent
+                error[j] = diff_percent
+        #print 'average %s' % average[i]
 
+
+    # calculus average error
+    average_error = 0
+    total_error = 0
+    for e in error:
+        total_error = total_error + e
+    average_error = total_error / len(test)
+    averageSet.append(average_error)
+    print 'average error rate %f ' % average_error
+
+#assert len(averageSet) == 100
+
+
+plt.plot(range(1, 101), averageSet, marker='o', color='r')
+plt.xlabel('K')
+plt.ylabel('error rate %')
+plt.show()
+
+"""
 plt.plot(range(0, len(test)), error, marker='o') 
 plt.xlabel('')
-plt.ylabel('error %')
+plt.ylabel('error rate %')
 #plt.ylim([0, 100])
 plt.xlim([0, len(test)])
 plt.show()
+"""
+
 ## step 3: show the result  
 #print "step 3: show the result..."  
 #showCluster(dataSet, k, centroids, clusterAssment)  
